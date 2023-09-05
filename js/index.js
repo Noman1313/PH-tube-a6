895944663a40cb7adc1b148475acd9f2a23df07b
Abdullah13130 <96126010+Abdullah13130@users.noreply.github.com>
2023-09-05 18:05:32 +0600

    sort by view

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let currentData  ;
+
 const handleCategory = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
@@ -17,14 +19,22 @@ const handleCategory = async () => {
 }
 
 const cardContainer = document.getElementById('card-container');
+const imageContainer = document.getElementById('image-container');
 const handleCategoryNews = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
+  currentData = data;
+  render(data);  
+
     
 
+}
+
+const render = (data) =>{
     if (data.status === true) {
 
         cardContainer.textContent = '';
+        imageContainer.textContent = '';
         const categoryNews = data.data;
 
 
@@ -33,35 +43,38 @@ const handleCategoryNews = async (categoryId) => {
             const div = document.createElement('div');
             div.classList = `card bg-base-100 shadow-xl`
             div.innerHTML = `
-        <div class="grid justify-items-end">
-            <img class="w-[100%] h-52 rounded-xl" src="${news.thumbnail}" alt="Shoes" />
-            <h3 class="bg-slate-700 rounded-md mt-[-30px] mr-3 text-white">${news.others.posted_date  ? 
-                convertSeconds(news.others.posted_date)
-                
-                : ''
-                
-                }</h3>    
-        </div>
-        <div class="flex gap-5 my-5">
-            <div>
-                <img class="w-10 h-10 rounded-full" src="${news.authors[0].profile_picture}" alt="">
-            </div>
-            <div>
-                <h2 class="text-xl font-bold">${news.title}</h2>
-                <div class="flex gap-2 items-center">
-                    <h2 class="text-lg">${news.authors[0].profile_name}</h2>
-                     
-                    <p>${news.authors[0].verified ? '<img src="images/blue badge.png" alt="">' : ''}</p> 
+                <div class="grid justify-items-end">
+                    <img class="w-[100%] h-52 rounded-xl" src="${news.thumbnail}" alt="Shoes" />
+                    <h3 class="bg-slate-700 rounded-md mt-[-30px] mr-3 text-white">${news.others.posted_date  ? 
+                        convertSeconds(news.others.posted_date)
+                        
+                        : ''
+                        
+                        }</h3>    
                 </div>
-                <h2 class="text-lg">${news.others.views} </h2>
-            </div>
-        </div>
-        `;
+                <div class="flex gap-5 my-5">
+                    <div>
+                        <img class="w-10 h-10 rounded-full" src="${news.authors[0].profile_picture}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="text-xl font-bold">${news.title}</h2>
+                        <div class="flex gap-2 items-center">
+                            <h2 class="text-lg">${news.authors[0].profile_name}</h2>
+                            
+                            <p>${news.authors[0].verified ? '<img src="images/blue badge.png" alt="">' : ''}</p> 
+                        </div>
+                        <h2 class="text-lg">${news.others.views} </h2>
+                    </div>
+                </div>
+            `;
             cardContainer.appendChild(div);
+            
         });
     }
     else {
+        
         cardContainer.textContent = '';
+        imageContainer.textContent = '';
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="flex justify-center">
@@ -69,12 +82,9 @@ const handleCategoryNews = async (categoryId) => {
         </div>
         <p class="text-2xl font-bold text-center">Oops!! Sorry, There is no content here</p>
          `
-        cardContainer.appendChild(div);
+         imageContainer.appendChild(div);
     }
-
 }
-
-
 
 handleCategory();
 handleCategoryNews("1000");
@@ -94,5 +104,20 @@ const convertSeconds = (seconds) => {
 
 
 
-
+document.getElementById('sort-by-view').addEventListener('click', function () {
+    currentData.data.sort((a, b) => {
+      const nameA = parseFloat(a.others.views.replace("K"))
+      const nameB = parseFloat(b.others.views.replace("K"))
+      if (nameA < nameB) {
+        return 1;
+      }
+      if (nameA > nameB) {
+        return -1;
+      }
+    
+      // names must be equal
+      return 0;
+    });
+    render(currentData);
+})
   
